332dc4a24d95671d279a3477f4ff0622ed055716
Marcelo Pinheiro Meireles <marcelopimeireles@gmail.com>
2020-06-01 16:48:12 +0100

    saga updateProfile fix

--- src/store/modules/user/sagas.js
+++ src/store/modules/user/sagas.js
@@ -7,13 +7,11 @@ import { updateProfileSuccess, updateProfileFailure } from './actions';
 
 export function* updateProfile({ payload }) {
   try {
-    const { name, email, ...rest } = payload.data;
+    const { name, email, avatar_id, ...rest } = payload.data;
 
-    const profile = {
-      name,
-      email,
-      ...(rest.oldPassword ? rest : {}),
-    };
+    const restProfile = rest.oldPassword ? rest : {};
+
+    const profile = { name, email, avatar_id, ...restProfile };
 
     const response = yield call(api.put, 'users', profile);
 
